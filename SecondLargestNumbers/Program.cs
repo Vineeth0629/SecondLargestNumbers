52f2aab306f28fbefbf60d977f428a388894d036
vineethrk <vineethrk@moback.com>
2022-09-21 17:59:33 +0530

    Secondlargestnumbersample

--- SecondLargestNumbers/Program.cs
+++ SecondLargestNumbers/Program.cs
@@ -12,15 +12,17 @@ public class Program
         Array.Reverse(array); // Reverse Sorting array value - descending 
         //to read value in arrary arrayname[indexvalue]
         Console.WriteLine("Second Highest Value In Array " + array[1]);
-
+        //
+        int a = Convert.ToInt32(Console.ReadLine());
+        int[] b = new int[a];
         foreach (var result in array)
         {
             Console.Write(result + " "); // Array values   
         }
 
-        for(int item=0; item<array.Length;item++)
-        {
-            Console.Write(array[item] + "  ");
-        }
+        //for(int item=0; item<array.Length;item++)
+        //{
+        //    Console.Write(array[item] + "  ");
+        //}
     }
 }
